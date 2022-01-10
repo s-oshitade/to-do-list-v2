05503988beec3330801d906b54f6ded2ca7e2986
Seun Oshitade <seunoshitade@Seuns-MacBook-Air.local>
2022-01-10 11:14:55 -0700

    Render databse items in the app

--- app.js
+++ app.js
@@ -1,13 +1,9 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const port = 3000;
-const date = require(__dirname + "/date.js");
-
 
 const app = express();
-
-const items = ["Code everyday", "Exercise before shower", "Give thanks"];
-const workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -17,30 +13,65 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-app.get('/', (req, res) => {
+mongoose.connect("mongodb://localhost:27017/todolistDB");
 
-  const day = date.getDate();
+const itemsSchema = {
+  name: String
+};
 
-  res.render("list", {
-    listTitle: day,
-    newListItems: items
-  });
+const Item = mongoose.model("Item", itemsSchema)
 
+const item1 = new Item({
+  name: "Welcome to your todolist!"
 });
 
+const item2 = new Item({
+  name: "Hit the + button to add a new item"
+});
+
+const item3 = new Item({
+  name: "<-- Hit this to delete an item."
+});
+
+const defaultItems = [item1, item2, item3]
+
+
+
+
+app.get('/', (req, res) => {
+
+  Item.find({}, function(err, foundItems){
+    if (foundItems.length === 0) {
+      Item.insertMany(defaultItems, function (err) {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Successfully saved default item to DB");
+        }
+      });
+      res.redirect("/");
+    } else {
+      res.render("list", {listTitle: "Today",newListItems: foundItems});
+    }
+  })
+});
+
+
 app.post('/', (req, res) => {
-  let item = req.body.newItem;
-  if (req.body.list === "Work List") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  const itemName = req.body.newItem;
+
+  const item = new Item({
+    name: itemName
+  })
+  item.save();
+  res.redirect("/")
 });
 
 app.get("/work", (req, res) => {
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+  res.render("list", {
+    listTitle: "Work List",
+    newListItems: workItems
+  });
 });
 
 app.get("/about", (req, res) => {

797dc51f9132bae3c91e15d9a77a927d202e318a
Seun Oshitade <seun.oshitade@gmail.com>
2022-01-13 15:32:33 +0000

    Host database on MongoDB Atlas

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(bodyParser.urlencoded({
 
 app.use(express.static("public"));
 
-mongoose.connect("mongodb://localhost:27017/todolistDB");
+mongoose.connect("mongodb+srv://s-oshitade:Test123@cluster0.rk3tg.mongodb.net/todolistDB");
 
 const itemsSchema = {
   name: String
